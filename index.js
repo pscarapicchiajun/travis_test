e0a26a2d6ff602aab6ca5780aba0d3f1ccc62dd6
Pedro Scarapicchia Junior <pscarapicchiajun@paypal.com>
2019-08-23 18:00:02 -0300

    bump v1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) =>
-  res.send('Hello from Chronos v0')
+  res.send('Hello from Chronos v1')
 );
 
 app.listen(80, () => {
